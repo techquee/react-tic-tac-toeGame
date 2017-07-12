30c8a4298f95701e287ce5bb31c0c4f4d7513011
hen <techbabbage@gmail.com>
2017-07-12 23:54:14 +0530

    commit

---
+++ src/client.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+	render(){
+	return(
+	<h1>working</h1>
+	);
+	}
+}
+
+const app = document.getElementId('app');
+
+ReactDOM.render(<Layout/>,app);
